6246bfe8020a60254ca616aadf499b0c678c69b0
Gustavo Gomes <gustxvogomxs@gmail.com>
2023-01-14 21:53:05 -0300

    ponto de restauração

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,6 @@ class StaticPagesController < ApplicationController
 
   def dashboard
   end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'static_pages/dashboard'
   devise_for :users
   root 'static_pages#landing_page'
+
+  
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
