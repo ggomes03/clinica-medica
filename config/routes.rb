83b6b29df84b4479de8e5b34ab1266f9f7f4ad9d
Gustavo Gomes <gustxvogomxs@gmail.com>
2023-01-19 19:15:18 -0300

    tela de agendamento - estilização

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   resources :agendamentos
-  get 'dashboard_pages/consultas'
-  get 'dashboard_pages/agendamentos'
-  get 'dashboard_pages/cirurgias'
+
 
   get 'static_pages/landing_page'
   get 'static_pages/dashboard'
 
-  get 'agendamento' => 'agendamento'
+ 
+
+
 
   devise_for :users
 
