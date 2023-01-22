9e784f9cacff04269d4a0bc300cb367bc8790555
Gustavo Gomes <gustxvogomxs@gmail.com>
2023-01-22 20:59:48 -0300

    estilizacao de erros no submit

--- app/models/agendamento.rb
+++ app/models/agendamento.rb
@@ -1,3 +1,12 @@
 class Agendamento < ApplicationRecord
   belongs_to :user
+
+  validates :nome, presence: true   
+  validates :medico, presence: true   
+  validates :cpf, presence: true, length: {minimum:11, maximum:11}
+  validates :especialidade, presence: true   
+  validates :data_consulta, presence: true   
+  validates :data_nascimento, presence: true   
+  
+
 end
